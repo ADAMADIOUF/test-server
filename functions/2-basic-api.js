bec0f5f76f5afd0b4c0f9bdddac707874cd0a7e9
ADAMADIOUF <adamadiouf2017@gmail.com>
2021-09-02 13:27:45 -0500

    fixe error

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,7 +1,10 @@
-
 const items = require("../assets/data")
 exports.handler = async (event, contex) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+
     statusCode: 200,
     body: JSON.stringify(items)
   }
