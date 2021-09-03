9a1f7370be4b31f45bdb95314325bd1c2799a01b
ADAMADIOUF <adamadiouf2017@gmail.com>
2021-09-03 04:50:19 -0500

    ecommerce done half

---
+++ functions/3-airtable.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appOAttcQCnLNcbU8')
+  .table('products')
+
+exports.handler = async (event, contex) => {
+ try {
+  const {records} = await airtable.list()
+const products = records.map((product)=> {
+ const {id} = product
+ const {name,image,price}= product.fields 
+ const url = image[0].url
+ return {id,name,url,price}
+})
+return {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+
+  statusCode: 200,
+  body: JSON.stringify(products),
+}  
+ } catch (error) {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+
+    statusCode: 500,
+    body: 'airtable',
+  }
+ }
+  
+}
---
+++ functions/3-product.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appOAttcQCnLNcbU8')
+  .table('products')
+exports.handler = async (event, contex) => {
+  const { id } = event.queryStringParameters
+  if (id) {
+    try {
+     const product = await airtable.retrieve(id)
+     if(product.error){
+      return {
+        statusCode: 404,
+        body: `no product with id ${id}`,
+      }
+     }
+     return {
+       statusCode: 200,
+       body: JSON.stringify(product),
+     }
+    } catch (error) {
+     return {
+      
+       statusCode: 500,
+       body: `server error`,
+     }
+    }
+  }
+ 
+  return {
+    statusCode: 400,
+    body: 'please provide product id',
+  }
+}
